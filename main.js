cbe03f5cc4725b3cdf5fcc037a73893a88c5adc0
pablo-jm <pablojime1998@gmail.com>
2025-02-18 10:33:54 +0100

    pre shapes

--- main.js
+++ main.js
@@ -3,3 +3,14 @@ const ctx = canvas.getContext('2d');
 const pauseBtn = document.getElementById('pauseButton');
 const scoreElement = document.getElementById('score');
 const highScoreElement = document.getElementById('highScore');
+
+const ROWS = 20, COLS = 10, SIZE = 20;
+let board = Array.from({ length: ROWS }, () => Array(COLS).fill(0));
+
+let isPaused = false;
+let gameInterval;
+let score = 0;
+let highScore = localStorage.getItem('highScore') || 0;
+highScoreElement.textContent = highScore;
+
+const SHAPES = 
