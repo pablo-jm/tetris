087768ac4e403e3952047fd1200eaaafce5cbf7e
pablo-jm <pablojime1998@gmail.com>
2025-02-18 09:58:31 +0100

    html and css done

---
+++ main.js
@@ -0,0 +1,5 @@
+const canvas = document.getElementById('tetris');
+const ctx = canvas.getContext('2d');
+const pauseBtn = document.getElementById('pauseButton');
+const scoreElement = document.getElementById('score');
+const highScoreElement = document.getElementById('highScore');
