03c97d75c9f60b07f7caab9d7b2e1dcf2ff86201
pablo-jm <pablojime1998@gmail.com>
2025-02-18 11:11:43 +0100

    drawing blocks

--- main.js
+++ main.js
@@ -13,4 +13,33 @@ let score = 0;
 let highScore = localStorage.getItem('highScore') || 0;
 highScoreElement.textContent = highScore;
 
-const SHAPES = 
+const SHAPES = [
+   [[1, 1, 1, 1]], //linea recta
+   [[1, 1], [1, 1]], //cuadrado
+   [[1, 1, 0], [0, 1, 1]], // Z
+   [[0, 1, 0], [1, 1, 1]], // T
+   [[0, 1, 1], [1, 1, 0]] // S
+];
+
+let currentPiece = {
+    shape: SHAPES[Math.floor(Math.random() * SHAPES.length)],
+    x: 3,
+    y: 0
+};
+
+
+function drawBoard(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    board.forEach((row, y) => row.forEach((value, x) => {
+        if(value) drawBlock(x, y, 'black');
+    }));
+}
+
+
+function drawBlock(x, y, color){
+    ctx.fillStyle = color;
+    ctx.fillRect(x * SIZE, y * SIZE, SIZE, SIZE);
+    ctx.strokeStyle = 'black';
+    ctx.strokeRect(x * SIZE, y * SIZE, SIZE, SIZE);
+}
+
